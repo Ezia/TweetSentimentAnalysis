896a52d8b344bb6745abbf56621a21656057647a
ValentinDORMANN <valentin.dormann@gmail.com>
2017-12-15 16:50:57 -0500

    checkstyle of StreamingRoute

--- StormTweetsSentimentD3Viz-master/src/main/java/org/p7h/storm/sentimentanalysis/cameljms/StreamingRoute.java
+++ StormTweetsSentimentD3Viz-master/src/main/java/org/p7h/storm/sentimentanalysis/cameljms/StreamingRoute.java
@@ -7,9 +7,8 @@ import org.apache.camel.builder.RouteBuilder;
  */
 public final class StreamingRoute extends RouteBuilder {
 
-	@Override
-	public final void configure() throws Exception {
-		from("activemq:my-queue")
-				.to("websocket://storm?sendToAll=true");
-	}
+  @Override
+  public final void configure() throws Exception {
+    from("activemq:my-queue").to("websocket://storm?sendToAll=true");
+  }
 }
