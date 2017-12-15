71f60bea7b5fb09ebc3c4f5aa1d82b8a2bfd114a
ValentinDORMANN <valentin.dormann@gmail.com>
2017-12-15 16:00:37 -0500

    Refactor and checkstyle of SpringJmsProvider

--- StormTweetsSentimentD3Viz-master/src/main/java/org/p7h/storm/sentimentanalysis/cameljms/SpringJmsProvider.java
+++ StormTweetsSentimentD3Viz-master/src/main/java/org/p7h/storm/sentimentanalysis/cameljms/SpringJmsProvider.java
@@ -7,30 +7,30 @@ import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
 
 public class SpringJmsProvider implements JmsProvider {
-    private static final long serialVersionUID = 2198261240300136509L;
-    private ConnectionFactory connectionFactory;
-    private Destination destination;
+  private static final long serialVersionUID = 2198261240300136509L;
+  private ConnectionFactory connectionFactory;
+  private Destination destination;
 
-    /**
-     * Constructs a <code>SpringJmsProvider</code> object given the name of a
-     * classpath resource (the spring application context file), and the bean
-     * names of a JMS connection factory and destination.
-     *
-     * @param context - the spring configuration file (classpath resource)
-     * @param connectionFactoryBean - the JMS connection factory bean name
-     * @param destinationBean - the JMS destination bean name
-     */
-    public SpringJmsProvider(ApplicationContext context, String connectionFactoryBean, String destinationBean){
-        this.connectionFactory = (ConnectionFactory)context.getBean(connectionFactoryBean);
-        this.destination = (Destination)context.getBean(destinationBean);
-    }
+  /**
+   * Constructs a <code>SpringJmsProvider</code> object given the name of a
+   * classpath resource (the spring application context file), and the bean
+   * names of a JMS connection factory and destination.
+   *
+   * @param context - the spring configuration file (classpath resource)
+   * @param connectionFactoryBean - the JMS connection factory bean name
+   * @param destinationBean - the JMS destination bean name
+   */
+  public SpringJmsProvider(ApplicationContext context, String connectionFactoryBean, String destinationBean) {
+    this.connectionFactory = (ConnectionFactory)context.getBean(connectionFactoryBean);
+    this.destination = (Destination)context.getBean(destinationBean);
+  }
 
-    public ConnectionFactory connectionFactory() throws Exception {
-        return this.connectionFactory;
-    }
+  public ConnectionFactory connectionFactory() throws Exception {
+    return this.connectionFactory;
+  }
 
-    public Destination destination() throws Exception {
-        return this.destination;
-    }
+  public Destination destination() throws Exception {
+    return this.destination;
+  }
 
 }
